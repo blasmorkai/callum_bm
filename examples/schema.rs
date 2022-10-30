ce66fb9cf1da302b19213a1e76ee51fb53470c12
Blas Morkai <blas.morkai@gmail.com>
2022-10-30 14:19:19 +0000

    Updating schema

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use callumbis::msg::{GetPollResponse, ExecuteMsg, InstantiateMsg, QueryMsg, MigrateMsg};
-use callumbis::state::{Config,SPoll};
+use callum_bm::msg::{GetPollResponse, ExecuteMsg, InstantiateMsg, QueryMsg, MigrateMsg};
+use callum_bm::state::{Config,SPoll};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,5 @@ pub mod helpers;
 pub mod msg;
 pub mod state;
 
+
 pub use crate::error::ContractError;
